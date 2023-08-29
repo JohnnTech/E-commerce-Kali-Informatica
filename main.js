100b5c5f9b1988eb7ded1dd811801be2cb8afde0
JohnnTech <johnntech01@gmail.com>
2023-08-29 00:45:25 -0300

    Inserindo os produtos com o JavaScript

---
+++ main.js
@@ -0,0 +1,33 @@
+const catalogo = [
+    {  
+        id: 1,
+        nome: "Notebook Gamer",
+        marca: "Toshiba",
+        preco: 1900,
+        nomeArquivoImagem: "NotebookGamer.jpg",
+    },
+    {
+        id: 2,
+        nome: "Teclado  USB Preto",
+        marca: "Brigid",
+        preco: 45,
+        nomeArquivoImagem: "Teclado.png",
+    },
+];
+
+for(const produtoCatalogo of catalogo){
+
+// const lista = ["info", "info1", "info2"]
+const cartaoProduto = `<div id="card-produto1">
+<img 
+    src="./assets/img/${produtoCatalogo.nomeArquivoImagem}" alt="Teclado"
+    style="height: 200px"
+/>
+<p>${produtoCatalogo.nome}</p>
+<p>${produtoCatalogo.marca}</p>
+<p>$${produtoCatalogo.preco}</p>
+<button>Adicionar</button>
+</div>`;
+
+document.getElementById("container-produto").innerHTML += cartaoProduto;
+}
